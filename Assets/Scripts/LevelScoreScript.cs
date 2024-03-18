35595952456cf7f28416e1b7b1650a2a3c9860ba
Edible <rwoznicki@edu.cdv.pl>
2024-03-18 23:20:39 +0100

    All the scripts commented

--- Assets/Scripts/LevelScoreScript.cs
+++ Assets/Scripts/LevelScoreScript.cs
@@ -1,28 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class LevelScoreScript : MonoBehaviour
+public class LevelScoreScript : MonoBehaviour //Handles the display of the current score on the UI display
 {
     public int levelScore;
     public TextMeshProUGUI text;
-    [SerializeField] private CurrentLevelInfo currentLevelInfo;
+    [SerializeField] private CurrentLevelInfo currentLevelInfo; //SO containing the information about the current level session
 
-    private void OnEnable()
+    private void OnEnable() //When this event is called this function will run to increase the score on the display
     {
         ObjectShippingBoxUse.LevelScoreIncreaseEvent += IncreaseScore;
     }
 
-    private void OnDisable()
+    private void OnDisable() //Lmao idk
     {
         ObjectShippingBoxUse.LevelScoreIncreaseEvent -= IncreaseScore;
     }
 
-    private void IncreaseScore(int levelScore, bool ribbon)
+    private void IncreaseScore(int levelScore, bool ribbon) //Runs the function with the current score the display and another discarded variable
     {
         text = GetComponent<TextMeshProUGUI>();
-        text.text = levelScore.ToString();
-        currentLevelInfo.currentLevelScore += levelScore;
+        text.text = levelScore.ToString(); //TMP text component is updated with the new level score
+        currentLevelInfo.currentLevelScore += levelScore; //SO with the information about the current level session is updated with the new level score
     }
 }
