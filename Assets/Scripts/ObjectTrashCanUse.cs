056462eed25b106addd8ebdc03fa365fb8e96edc
Edible <rwoznicki@edu.cdv.pl>
2024-03-18 04:39:39 +0100

    Half the scripts commented

--- Assets/Scripts/ObjectTrashCanUse.cs
+++ Assets/Scripts/ObjectTrashCanUse.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectTrashCanUse : MonoBehaviour
 {
-    [SerializeField] private GameObject playerTrigger;
+    [SerializeField] private GameObject playerTrigger; //Player collider object
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit(Collider other) //When the player is no longer in front of the trashcan the reference is removed so it can no longer be used
     {
         if (other.CompareTag("PlayerTrigger"))
         {
@@ -14,7 +12,7 @@ public class ObjectTrashCanUse : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) //When the player approaches the trashcan the reference is made to the slot where the player holds an item
     {
         if (other.CompareTag("PlayerTrigger"))
         {
@@ -24,13 +22,12 @@ public class ObjectTrashCanUse : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerTrigger != null)
+        if (Input.GetKeyDown(KeyCode.E) && playerTrigger != null) //When the player presses E & has an item
         {
-            Destroy(playerTrigger.transform.GetChild(0).gameObject);
-
-            PlayerPickUpItem playerPickUpItem = playerTrigger.GetComponent<PlayerPickUpItem>();
-            playerPickUpItem.heldItem = null;
-            playerPickUpItem.areHandsFull = false;
+            Destroy(playerTrigger.transform.GetChild(0).gameObject); //First child object (presumably the held item) of the player item slot is destroyed
+            PlayerPickUpItem playerPickUpItem = playerTrigger.GetComponent<PlayerPickUpItem>(); //References the script that handles the player picking up and putting down items
+            playerPickUpItem.heldItem = null; //The player no longer thinks they hold an item
+            playerPickUpItem.areHandsFull = false; //The player can pick up an item again
         }
     }
 }
--- Assets/Scripts/UILevelCompleteScore.cs
+++ Assets/Scripts/UILevelCompleteScore.cs
@@ -1,15 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
 
 public class UILevelCompleteScore : MonoBehaviour
 {
-    [SerializeField] private CurrentLevelInfo currentLevelInfo;
-    private TextMeshProUGUI text;
+    [SerializeField] private CurrentLevelInfo currentLevelInfo; //SO containing the information about the current level played
+    private TextMeshProUGUI text; //Reference to self
 
-    void Awake()
+    void Awake() //When the Level Finish scene loads the script adds the level score to the text object
     {
         text = GetComponent<TextMeshProUGUI>();
         text.text = "Score: " + currentLevelInfo.currentLevelScore;
