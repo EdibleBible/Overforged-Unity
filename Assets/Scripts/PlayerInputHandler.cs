effae7d25595cf53924bc64a219e57da95485899
Ula <ula.mikolajczyk06@gmail.com>
2024-05-16 00:14:52 +0200

    2ppl movement in progress NOT FINISHED

---
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private Mover mover;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var movers = FindObjectsOfType<Mover>();
+        if (movers.Any() && playerInput !=null)
+        {
+            var index = playerInput.playerIndex;
+            mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        }
+        else
+        {
+            mover = null;
+        }
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        if(mover != null) 
+        mover.SetInputVector(context.ReadValue<Vector2>());
+    }
+
+}
